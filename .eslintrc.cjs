2e26d03bc6dd2f3ad140be993730fb9ce2aabcdf
Kseylo <alekseysarychev29@gmail.com>
2024-04-24 19:59:52 +0700

    Added font, base styles, vite config and fixed eslint error with vite config.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,16 @@ const {
 
 module.exports = configure({
     allowDebug: process.env.NODE_ENV !== "production",
-
     presets: [
         presets.imports(),
         presets.node(),
         presets.prettier(),
-        presets.typescript()
-    ]
+        presets.typescript({
+            root: './',
+            tsconfig: 'tsconfig.json',
+        })
+    ],
+    parserOptions: {
+      project: ['./tsconfig.json'],
+    }
 });
