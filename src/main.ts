322312cfe19c96719096d3095c11f278fb01abb1
Kseylo <alekseysarychev29@gmail.com>
2024-04-25 15:46:23 +0700

    Renamed from index.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { Login } from '@/pages/login'
 import { SignIn } from '@/pages/sign-in'
 
 document.addEventListener('DOMContentLoaded', () => {
+  console.log(SignIn)
   const root = document.querySelector('#root')!
-  const button = Handlebars.compile(Login())
-  root.innerHTML = button({})
+  // const signInTemplate = Handlebars.compile(SignIn())
+  const loginTemplate = Handlebars.compile(Login())
+  // root.innerHTML = signInTemplate({})
+  root.innerHTML = loginTemplate({})
 })
