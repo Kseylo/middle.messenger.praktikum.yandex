df3f38eeb3f026c305a9332b614cf5b5fa1d2143
Kseylo <alekseysarychev29@gmail.com>
2024-04-25 16:32:06 +0700

    RegisterPartials from UI folder

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import Handlebars from 'handlebars'
+import HandlebarsRuntime from 'handlebars/runtime'
 import { Login } from '@/pages/login'
-import { SignIn } from '@/pages/sign-in'
+// import { SignIn } from '@/pages/sign-in'
+import * as UI from '@/shared/ui'
+
+Object.entries(UI).forEach(([name, partial]) =>
+  HandlebarsRuntime.registerPartial(name, partial),
+)
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log(SignIn)
   const root = document.querySelector('#root')!
   // const signInTemplate = Handlebars.compile(SignIn())
   const loginTemplate = Handlebars.compile(Login())
