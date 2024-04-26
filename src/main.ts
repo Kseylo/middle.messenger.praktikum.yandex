b66a65d7bef8a0d520871a6ddd1e67997a60b835
Kseylo <alekseysarychev29@gmail.com>
2024-04-26 11:50:10 +0700

    Initially completed error page

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import Handlebars from 'handlebars'
 import HandlebarsRuntime from 'handlebars/runtime'
+import { Error } from '@/pages/error'
 import { Login } from '@/pages/login'
 // import { SignIn } from '@/pages/sign-in'
 import * as UI from '@/shared/ui'
@@ -11,7 +12,14 @@ Object.entries(UI).forEach(([name, partial]) =>
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.querySelector('#root')!
   // const signInTemplate = Handlebars.compile(SignIn())
-  const loginTemplate = Handlebars.compile(Login())
+  // const loginTemplate = Handlebars.compile(Login())
+  const errorTemplate = Handlebars.compile(
+    Error({
+      code: 404,
+      description: 'Страница не найдена',
+    }),
+  )
   // root.innerHTML = signInTemplate({})
-  root.innerHTML = loginTemplate({})
+  // root.innerHTML = loginTemplate({})
+  root.innerHTML = errorTemplate({})
 })
