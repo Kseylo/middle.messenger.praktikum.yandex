d7bba12ef319429b1c5a015d6aa27d649f726985
Kseylo <alekseysarychev29@gmail.com>
2024-04-30 20:54:53 +0700

    Node version

--- server/express.js
+++ server/express.js
@@ -8,6 +8,10 @@ const PORT = 3000
 
 app.use(express.static(path.resolve(__dirname, '..', 'dist')))
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'dist', 'index.html'))
+})
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}\n`)
   console.log(`http://localhost:${PORT}`)
