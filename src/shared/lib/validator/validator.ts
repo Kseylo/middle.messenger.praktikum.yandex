eb606010290254eb1e8842b812870fccc2726b70
Kseylo <alekseysarychev29@gmail.com>
2024-05-22 11:33:16 +0700

    Changed validation logic

--- src/shared/lib/validator/validator.ts
+++ src/shared/lib/validator/validator.ts
@@ -1,3 +1,5 @@
+import { InputWithLabel } from '@/shared/ui'
+
 export enum FIELDS {
   LOGIN = 'login',
   PASSWORD = 'password',
@@ -6,45 +8,45 @@ export enum FIELDS {
 }
 
 export class Validator {
-  private _validate(input: string, regex: RegExp, errorMessage: string) {
-    if (regex.test(input)) {
-      return { isValid: true, errorMessage: '' }
+  static rules = {
+    [FIELDS.LOGIN]: {
+      regex: /^(?![0-9]+$)[A-Za-z0-9_-]{3,20}$/,
+      errorMessage: 'Некорректный логин',
+    },
+    [FIELDS.PASSWORD]: {
+      regex: /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,40}$/,
+      errorMessage: 'Некорректный пароль',
+    },
+    [FIELDS.FIRST_NAME]: {
+      regex: /^[A-ZА-ЯЁ][a-zA-Zа-яА-ЯёЁ-]*$/,
+      errorMessage: 'Некорректное имя',
+    },
+    [FIELDS.SECOND_NAME]: {
+      regex: /^[A-ZА-ЯЁ][a-zA-Zа-яА-ЯёЁ-]*$/,
+      errorMessage: 'Некорректная фамилия',
+    },
+  }
+
+  static validate(name: FIELDS, value: string) {
+    return {
+      isValid: this.rules[name].regex.test(value),
+      errorMessage: this.rules[name].errorMessage,
     }
-    return { isValid: false, errorMessage }
   }
 
-  private _validateLogin(login: string) {
-    const loginRegex = /^(?![0-9]+$)[A-Za-z0-9_-]{3,20}$/
-    return this._validate(login, loginRegex, 'Некорректный логин')
-  }
-
-  private _validatePassword(password: string) {
-    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,40}$/
-    return this._validate(password, passwordRegex, 'Некорректный пароль')
-  }
-
-  private _validateFirstName(name: string) {
-    const nameRegex = /^[A-ZА-ЯЁ][a-zA-Zа-яА-ЯёЁ-]*$/
-    return this._validate(name, nameRegex, 'Некорректное имя')
-  }
-
-  private _validateSecondName(name: string) {
-    const nameRegex = /^[A-ZА-ЯЁ][a-zA-Zа-яА-ЯёЁ-]*$/
-    return this._validate(name, nameRegex, 'Некорректная фамилия')
-  }
-
-  validate(input: string, value: string) {
-    switch (input) {
-      case FIELDS.LOGIN:
-        return this._validateLogin(value)
-      case FIELDS.PASSWORD:
-        return this._validatePassword(value)
-      case FIELDS.FIRST_NAME:
-        return this._validateFirstName(value)
-      case FIELDS.SECOND_NAME:
-        return this._validateSecondName(value)
-      default:
-        return { isValid: true, errorMessage: '' }
-    }
+  static validateInput(...inputs: InputWithLabel[]) {
+    inputs.forEach((inputElement) => {
+      const input = inputElement.getContent().querySelector('input')
+      if (input) {
+        const { isValid, errorMessage } = this.validate(
+          input.name as FIELDS,
+          input.value,
+        )
+        inputElement.setProps({
+          ...inputElement.props,
+          errorMessage: isValid ? '' : errorMessage,
+        })
+      }
+    })
   }
 }
